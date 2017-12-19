7fbe65d6728eed5970673d9f2d2b5bb02df79d57
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2017-12-18 20:37:43 -0600

    Added game initializer / basic start button functionality

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+
+//=============
+//App Variables
+//=============
+
+var questions = [
+	{
+		question : "",
+		guesses : "",
+		answer : ""
+	},
+	{
+		question : "",
+		guesses : "",
+		answer : ""
+	},
+	{
+		question : "",
+		guesses : "",
+		answer : ""
+	},
+	{
+		question : "",
+		guesses : "",
+		answer : ""
+	},
+	{
+		question : "",
+		guesses : "",
+		answer : ""
+	}
+];
+
+var number = 30;
+
+
+
+//=========
+//Functions
+//=========
+
+
+
+//======
+//Events
+//======
+
+$('#start-button').on('click', function initializeGame() {
+	$('.start-screen').css('display', 'none');
+	$('.game-screen').css('display', 'initial');
+});
+
+//=================
+//Initialize Script
+//=================
+
+$(document).ready(function() {
+
+
+});
