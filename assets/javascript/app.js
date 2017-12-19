46eb4526f4154e3ce2b572d65371a30412fc7d61
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2017-12-19 09:43:37 -0600

    Timer functionality added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,13 +33,33 @@ var questions = [
 
 var number = 30;
 
-
+var intervalId;
 
 //=========
 //Functions
 //=========
 
+function beginCountdown() {
+	intervalId = setInterval(decrement, 1000);
+};
 
+function decrement() {
+	number--;
+	$('#countdown').html('<h2>' + 'The game will end in ' + number + ' seconds.' + '</h2>');
+	if (number === 0) {
+		showResults();
+	};
+};
+
+function showResults() {
+	stopTimer();
+	$('.game-screen').css('display', 'none');
+	$('.results-screen').css('display', 'initial');
+};
+
+function stopTimer() {
+	clearInterval(intervalId);
+};
 
 //======
 //Events
@@ -48,7 +68,16 @@ var number = 30;
 $('#start-button').on('click', function initializeGame() {
 	$('.start-screen').css('display', 'none');
 	$('.game-screen').css('display', 'initial');
+
+	beginCountdown();
+
+	/*if (number === 0) {
+		showResults();
+	};*/
 });
+
+$('#done').on('click', showResults);
+
 
 //=================
 //Initialize Script
