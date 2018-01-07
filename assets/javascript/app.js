7ebd9719512d76a675752a80299fbbf4c81340a3
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2018-01-07 17:58:05 -0600

    Optimized code to push text from objects into HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,28 +6,28 @@
 var questions = [
 	{
 		question : "Tokyo is comprised of 23 wards. Which of the following is NOT a Tokyo ward?",
-		guesses : ["Shinjuku", "Edogawa", "Chiyoda"],
-		answer : "Yokohama"
+		guesses : ["Shinjuku", "Edogawa", "Chiyoda", "Yokohama"],
+		indexAnswer: 3
 	},
 	{
 		question : "Which is the largest of Japan's main islands?",
-		guesses : ["Hokkaido", "Shikoku", "Kyushu"],
-		answer : "Honshu"
+		guesses : ["Hokkaido", "Honshu", "Shikoku", "Kyushu"],
+		indexAnswer : 1
 	},
 	{
 		question : "A popular tourist destination, this city served as the Japanese capital from 794 A.D. until 1868.",
-		guesses : ["Kamakura", "Osaka", "Des Moines, Iowa"],
-		answer : "Kyoto"
+		guesses : ["Kamakura", "Osaka", "Kyoto", "Des Moines, Iowa"],
+		indexAnswer : 2
 	},
 	{
 		question : "This city was founded by Jesuit missionaries in the 16th century and the only place where the presence of foreigners was tolerated for over 400 years.",
-		guesses : ["Shibuya", "Macao", "Kobe"],
-		answer : "Nagasaki"
+		guesses : ["Shibuya", "Nagasaki", "Macao", "Kobe"],
+		indexAnswer : 1
 	},
 	{
 		question : "The southernmost of Japanese islands is home to the Ryukyu culture and was the stage of the bloodiest battle in the Pacific War.",
-		guesses : ["Hiroshima", "Australia", "Saipan"],
-		answer : "Okinawa"
+		guesses : ["Hiroshima", "Australia", "Saipan", "Okinawa"],
+		indexAnswer : 3
 	}
 ];
 
@@ -87,6 +87,7 @@ $(document).ready(function() {
 		event.preventDefault();
 		
 		showResults();
+
 		console.log(correct);
 		console.log(incorrect);
 		console.log(unanswered);
@@ -94,6 +95,8 @@ $(document).ready(function() {
 	});
 
 	function calculateScore() {
+		
+		
 		$('#question-1').each(function() {
 			if ($('#correct').prop('checked')) {
 				scoreUp();
@@ -171,42 +174,13 @@ $(document).ready(function() {
 	//==========================
 
 	$('#start-button').on('click', function initializeGame() {
-
-		//Adding text to the first question and its answers
-		$('#q1-q').html(questions[0].question);
-		$('#q1-1').html(questions[0].answer);
-		$('#q1-2').html(questions[0].guesses[0]);
-		$('#q1-3').html(questions[0].guesses[1]);
-		$('#q1-4').html(questions[0].guesses[2]);
-
-		//And now the second question and its answers
-		$('#q2-q').html(questions[1].question);
-		$('#q2-1').html(questions[1].guesses[0]);
-		$('#q2-2').html(questions[1].answer);
-		$('#q2-3').html(questions[1].guesses[1]);
-		$('#q2-4').html(questions[1].guesses[2]);
-
-		//And so on...
-		$('#q3-q').html(questions[2].question);
-		$('#q3-1').html(questions[2].guesses[0]);
-		$('#q3-2').html(questions[2].guesses[1]);
-		$('#q3-3').html(questions[2].answer);
-		$('#q3-4').html(questions[2].guesses[2]);
-
-		//Question 4
-		$('#q4-q').html(questions[3].question);
-		$('#q4-1').html(questions[3].guesses[0]);
-		$('#q4-2').html(questions[3].guesses[1]);
-		$('#q4-3').html(questions[3].guesses[2]);
-		$('#q4-4').html(questions[3].answer);
-
-		//Question 5
-		$('#q5-q').html(questions[4].question);
-		$('#q5-1').html(questions[4].guesses[0]);
-		$('#q5-2').html(questions[4].guesses[1]);
-		$('#q5-3').html(questions[4].answer);
-		$('#q5-4').html(questions[4].guesses[2]);
-
+		// Adds text to HTML elements
+		$('#question-forms').find('form').each(function(i) {
+			$(this).find('h3').html(questions[i].question);
+			$(this).find('.form-check-label').each(function(j) {
+				$(this).append(questions[i].guesses[j]);
+			});
+		});
 
 		//Changes the display to game screen
 		$('.start-screen').css('display', 'none');
